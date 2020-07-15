cfa126ebf84b1f23927c2c07e0d3f641a0a3fa2d
Vijay.Yeruva <Yeruva.Vijay@gmail.com>
2020-07-15 02:35:03 -0400

    added home page logos

--- js/home.js
+++ js/home.js
@@ -28,7 +28,7 @@ request.onload = function(){
             content +='<div class = "col-xl-4 col-lg-6 col-md-12">'+
                         '<a target = "_blank" href = "'+ element.source +'">'+
                             '<div class = "home-logo-container">' +
-                                '<img class = "home-logo" src = "assets/logo/home/' + element.logo+ '">'+
+                                '<img class = "home-logo" src = "assets/logos/home/' + element.logo+ '">'+
                                 '<p>'+element.content+'</p>' +
                             '</div>'+
                         '</a>'+
