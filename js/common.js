c268a1ec86bced16457c10942585ba841d3deb70
Vijay.Yeruva <Yeruva.Vijay@gmail.com>
2020-07-14 21:00:42 -0400

    added top mega menu

---
+++ js/common.js
@@ -0,0 +1,6 @@
+let addsidemenu = function(pageTitle){
+    let sidemenuItems = [{"item": "HOME", "link": "home.html"},{"item":"ABOUT", "link":""}]
+
+    let sidemenu = document.getElementById('navigation-bar');
+
+}
